e6239440c46012f98314b86b330b97dc1532ec49
viniciusyt <vinicius13terra@gmail.com>
2026-07-16 14:10:11 -0300

    update led-pwm

--- Exp8-LabProc/led-pwm.py
+++ Exp8-LabProc/led-pwm.py
@@ -3,21 +3,19 @@ import time
 
 LED_PIN = 17
 
-# Utiliza a numeração BCM: GPIO17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-# Frequência inicial
 pwm = GPIO.PWM(LED_PIN, 1)
 
 try:
     pwm.start(50)
 
-    frequencias = [1, 5, 10, 50, 100, 500, 1000]
+    frequencies = [2, 20, 200, 2000]
 
-    for frequencia in frequencias:
-        print(f"Testando frequência de {frequencia} Hz")
-        pwm.ChangeFrequency(frequencia)
+    for frequency in frequencies:
+        print(f"Testando frequência de {frequency} Hz")
+        pwm.ChangeFrequency(frequency)
         time.sleep(5)
 
 finally:
